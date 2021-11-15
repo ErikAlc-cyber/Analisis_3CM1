8203529205e1e3cbf083998f0596e87c0e071d24
ErikAlc <erik.senseya@gmail.com>
2021-11-15 15:23:08 -0600

    Creacion de Main, ejecuta todos los programas secuancialmente

---
+++ Practica_4/main.py
@@ -0,0 +1,16 @@
+import os
+
+def main():
+    os.system('systeminfo')
+    print("/////////*************//////////////")
+    os.system('python fibonacci.py')
+    print("/////////*************//////////////")
+    os.system('python CoeBin.py')
+    print("/////////*************//////////////")
+    os.system('python Mochila.py')
+    print("/////////*************//////////////")
+    os.system('python LCS.py')
+    
+
+if __name__ == "__main__":
+    main()
